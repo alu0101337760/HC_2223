2d06eef81eca1fd510e69dfbe679df4e9071b4ec
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-30 16:00:58 +0000

    Start: Added basic code structure
    Added basic tools to start working with node, typescript and testing with jest. Added also prettier and tslint for formatting and linting

---
+++ Code/src/__tests__/example.test.ts
@@ -0,0 +1,11 @@
+// Example test. Run with `npm test`
+
+describe("Example", () => {
+  test("Should work", () => {
+    expect(true).toBe(true);
+  });
+
+  test("Should fail", () => {
+    expect(true).toBe(false);
+  });
+});
---
+++ Code/src/index.ts
@@ -0,0 +1,4 @@
+// Hello world example. Run with `npm start`
+
+const greeter = (greetingMessage: string) => console.log(greetingMessage);
+greeter("Hello World");
