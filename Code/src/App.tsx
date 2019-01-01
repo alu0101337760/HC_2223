d6b349af759e721e95a596cb9cb84d0319d8707d
Carlos Domínguez García <carlosdg97@gmail.com>
2019-01-01 18:27:30 +0000

    Feat: Submittable Textarea component
    Added a component for letting the user submit the input

--- Code/src/App.tsx
+++ Code/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.scss";
 import Graph from "react-graph-vis";
+import SubmittableTextarea from "./components/SubmittableTextarea";
 
 interface INode {
   id: number;
@@ -12,13 +13,18 @@ interface IEdge {
   to: number;
 }
 
+interface IGraph {
+  nodes: INode[];
+  edges: IEdge[];
+}
+
 const graph = {
   nodes: [
-    { id: 1, label: "Node 1", x: 0, y: 0 },
-    { id: 2, label: "Node 2", x: 0, y: 100 },
-    { id: 3, label: "Node 3", x: 100, y: 0 },
-    { id: 4, label: "Node 4", x: 100, y: 100 },
-    { id: 5, label: "Node 5", x: 250, y: 250 }
+    { id: 1, group: "g1", label: "Node 1", x: 0, y: 0 },
+    { id: 2, group: "g1", label: "Node 2", x: 0, y: 100 },
+    { id: 3, group: "g1", label: "Node 3", x: 100, y: 0 },
+    { id: 4, group: "g1", label: "Node 4", x: 100, y: 100 },
+    { id: 5, group: "g1", label: "Node 5", x: 250, y: 250 }
   ],
   edges: [
     { from: 1, to: 2 },
@@ -40,23 +46,52 @@ const options = {
       }
     }
   },
+  groups: {
+    g1: { color: { background: "red" } }
+  },
   physics: {
     enabled: false
   }
 };
 
 const events = {
-  select: function(event: { nodes: INode[]; edges: IEdge[] }) {
+  select: function(event: IGraph) {
     const { nodes, edges } = event;
     console.log({ nodes, edges });
   }
 };
 
 class App extends Component {
+  onNewProblemInstance = (value: string): boolean => {
+    console.log({ value });
+
+    return false;
+  };
+
   render() {
     return (
       <div className="container">
-        <Graph graph={graph} options={options} events={events} />
+        <SubmittableTextarea onSubmit={this.onNewProblemInstance} />
+        <div style={{ height: "80vh", display: "flex" }}>
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              border: "1px solid black"
+            }}
+          >
+            <Graph graph={graph} options={options} events={events} />
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              border: "1px solid black"
+            }}
+          >
+            <Graph graph={graph} options={options} events={events} />
+          </div>
+        </div>
       </div>
     );
   }
