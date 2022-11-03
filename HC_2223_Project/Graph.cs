26df2e7379e3288c16f2014253dcdd547c76ebfa
Juan García Santos <alu0101325583@ull.edu.es>
2022-11-03 19:41:30 +0000

    Added input parsing and ToString method of Graph

--- HC_2223_Project/Graph.cs
+++ HC_2223_Project/Graph.cs
@@ -20,13 +20,24 @@ namespace HC_2223_Project
 
         private void InitializeFromFile(string filename)
         {
-
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            numberOfVertex = int.Parse(lines[0]);
+            arcs = new HashSet<(int, int)>();
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                string[] line = lines[i].Split(' ');
+                arcs.Add((int.Parse(line[0]), int.Parse(line[1])));
+            }
         }
 
         public override string ToString()
         {
-            return "Not implemented yet";
+            string result = numberOfVertex.ToString() + System.Environment.NewLine;
+            foreach ((int, int) arc in arcs)
+            {
+                result += arc.Item1.ToString() + " " + arc.Item2.ToString() + System.Environment.NewLine;
+            }
+            return result;
         }
-
     }
 }
--- HC_2223_Project/Main.cs
+++ HC_2223_Project/Main.cs
@@ -5,7 +5,9 @@ namespace HC_2223_Project
     {
         public static void Main(string filename)
         {
-            Console.WriteLine(VCToHCConverter.VC2HC(new Graph(filename)));
+            //Console.WriteLine(VCToHCConverter.VC2HC(new Graph(filename), 3));
+            Graph grafo = new Graph(filename);
+            Console.WriteLine(grafo.ToString());
         }
     }
 }
