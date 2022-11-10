b41750f0fdf6beab5af0344f58d4e70060aef482
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2022-11-10 11:25:23 +0000

    Modified dir structure to enable debugging, index out of range bug found

--- HC_2223_Project/Main.cs
+++ HC_2223_Project/Main.cs
@@ -3,10 +3,10 @@ namespace HC_2223_Project
 {
     public class Program
     {
-        public static void Main(string filename)
+        public static void Main(String[] args)
         {
             //Console.WriteLine(VCToHCConverter.VC2HC(new Graph(filename), 3));
-            Graph grafo = new Graph(filename);
+            Graph grafo = new Graph(args[0]);
             Console.WriteLine(grafo.ToString());
         }
     }
